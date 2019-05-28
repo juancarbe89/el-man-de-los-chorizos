dc13c30fdb080a1673daec8e08312df981ca261a
Juan Cardenas <juancarbe89@gmail.com>
2019-05-28 19:03:43 +1000

    Implemented Internationalization I18n and switch language menu and added OG meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_action :set_locale
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" } ### Change
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+    # Rails.application.routes.default_url_options[:locale] = I18n.locale
+  end
+
+  def default_url_options(_options = {})
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
 
-  resources :customers, only: [:index, :show, :edit, :update] do
-    resources :orders, only: [:index, :new, :create, :edit, :update]
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+
+    devise_for :customers
+    root to: 'pages#home'
+    resources :customers, only: [:index, :show, :edit, :update] do
+      resources :orders, only: [:index, :new, :create, :edit, :update]
+    end
+
+    # General
+    get '/about', to: 'pages#about', as: :about
+    get '/how', to: 'pages#how', as: :how
+
+    # Customer
+    get '/myprofile', to: 'customers#edit', as: :edit_profile
+    patch '/myprofile', to: 'customers#update', as: :update_profile
+    get '/myorders', to: 'orders#index', as: :my_orders
+
+    # DeliveryGuy
+    get '/orders/:id/placed', to: 'orders#placed', as: :placed_order
+    get '/orders/:id/delivering', to: 'orders#delivering', as: :delivering_order
+    get '/orders/:id/delivered', to: 'orders#delivered', as: :delivered_order
   end
-
-  # General
-  get '/about', to: 'pages#about', as: :about
-  get '/how', to: 'pages#how', as: :how
-
-  # Customer
-  get '/myprofile', to: 'customers#edit', as: :edit_profile
-  patch '/myprofile', to: 'customers#update', as: :update_profile
-  get '/myorders', to: 'orders#index', as: :my_orders
-
-  # DeliveryGuy
-  get '/orders/:id/placed', to: 'orders#placed', as: :placed_order
-  get '/orders/:id/delivering', to: 'orders#delivering', as: :delivering_order
-  get '/orders/:id/delivered', to: 'orders#delivered', as: :delivered_order
+  # match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+  # match '', to: redirect("/#{I18n.default_locale}")
 end
