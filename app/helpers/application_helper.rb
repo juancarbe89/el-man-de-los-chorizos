dca47df1cb4c3cbc561f6a5d78e1cb9637ec0604
Juan Cardenas <juancarbe89@gmail.com>
2019-05-29 09:51:40 +1000

    Added method in application_helper for identify device of user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active?(link_path)
     current_page?(link_path) ? "active" : ""
   end
+
+  def mobile_device
+    agent = request.user_agent
+    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+    return "mobile" if agent =~ /Mobile/
+
+    return "desktop"
+  end
 end
